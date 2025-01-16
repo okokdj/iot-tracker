7a15ce9ffe5513eee1fc32e1359a465449f580c0
okokdj <okuna.dorjbayar@student.ap.be>
2025-01-16 14:33:54 +0100

    add post endpoint

--- app/api/webhook/route.js
+++ app/api/webhook/route.js
@@ -1,3 +1,14 @@
-export async function GET() {
-  return Response.json({ message: 'Hello World&&&' })
+export async function POST(request) {
+  let data = null
+  try {
+    data = await request.json()
+    // Process the webhook payload
+    console.log(data)
+    // save it in the db
+  } catch (error) {
+    return new Response(`Webhook error: ${error.message}`, {
+      status: 400,
+    })
+  }
+  return Response.json({ data })
 }
